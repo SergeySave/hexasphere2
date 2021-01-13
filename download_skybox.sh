541301b086963a5823fb57c38bfd638262754cc6
SergeySave <sergeysav.nn@gmail.com>
2021-01-13 15:13:13 -0500

    adds skybox download script
    Former-commit-id: b2602b9d209be1bb347c73b797313312830ba7fd

---
+++ download_skybox.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+cd src/main/resources/skybox
+
+wget https://www.dropbox.com/s/bhlxvxl8w43ig5t/nasa1k.ktx
+wget https://www.dropbox.com/s/u52qrz9c902zeln/nasa2k.ktx
