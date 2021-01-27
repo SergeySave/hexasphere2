5f2cc0381517539ce616510b0c7753ccd7b254ea
SergeySave <sergeysav.nn@gmail.com>
2021-01-26 22:00:30 -0500

    moves skybox download into gradle build

--- download_skybox.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$0")"
-cd src/main/resources/skybox
-
-wget https://www.dropbox.com/s/bhlxvxl8w43ig5t/nasa1k.ktx
-wget https://www.dropbox.com/s/u52qrz9c902zeln/nasa2k.ktx
